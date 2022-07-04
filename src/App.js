82b7adb34655256e2d592c4fd5f199b3664f748a
rayanelsiddig <rayanelsiddig@sellyourmotors.com>
2022-07-04 20:19:46 +0200

    Enrollment System

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import StudentList from './componets/StudentList';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Enrollment System';
+  });
   return (
     <div className="container">
       <StudentList/>
